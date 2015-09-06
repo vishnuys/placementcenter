af45ffb2eb0d0d868cbe1b8e0d698a53cdd6715c
Vishnu Y S <vishnu.ys.007@gmail.com>
2015-09-06 21:20:02 +0530

    Removed unwanted directory

--- Attendance/pages/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-$con=mysql_connect($servername,$username,$password) or die("Error Conncting");
-$sel=mysql_select_db("attendance") or die("Database does not exist");
-?>
